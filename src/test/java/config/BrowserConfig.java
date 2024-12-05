16e46c6c53390bf2a608db964e6c74a36327fa35
Test DG User <Test DG Email>
2024-12-05 20:54:43 +0300

    added secret.properties

--- src/test/java/config/BrowserConfig.java
+++ src/test/java/config/BrowserConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
+        "file:/tmp/secret.properties",
         "classpath:${env}.properties",
         "classpath:local.properties"
 })
